1a366181878e59db9d691edea56ed1de83ce02a4
Kurtis Hoang <KurtisHoang@sfs-wifi-dhcp-10-143-4-33.wifi.sfsu.edu>
2019-02-28 14:37:36 -0800

    tweet with character limit, fav, and retweet done

--- Twitter_starter/Twitter/HomeTableViewController.swift
+++ Twitter_starter/Twitter/HomeTableViewController.swift
@@ -23,17 +23,25 @@ class HomeTableViewController: UITableViewController {
         myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
         //set tableview's refreshControl
         tableView.refreshControl = myRefreshControl
+        //automatic cell height
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweet()
     }
     
     @objc func loadTweet()
     {
         
-        numberOfTweets = 20
+        numberOfTweets = 60
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count": numberOfTweets] //["count": 20, "id" : 34]
         
         //call api for multiple disctionaries of tweets
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
             
             //remove all current tweets
             self.tweetArray.removeAll()
@@ -61,7 +69,7 @@ class HomeTableViewController: UITableViewController {
         let myParams = ["count": numberOfTweets]
         
         //call api for multiple disctionaries of tweets
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
             
             //remove all current tweets
             self.tweetArray.removeAll()
@@ -105,14 +113,51 @@ class HomeTableViewController: UITableViewController {
         let user = tweetArray[indexPath.row]["user"] as! NSDictionary
         
         //set username and tweet content
-        cell.userNameLabel.text = user["name"] as! String
-        cell.tweetContentLabel.text = tweetArray[indexPath.row]["text"] as! String
+        cell.userNameLabel.text = (user["name"] as! String)
+        cell.tweetContentLabel.text = (tweetArray[indexPath.row]["text"] as! String)
         
         //get imageURL and set it to cell profileImageView using AlamoFireImage
         let imageUrl = URL(string: user["profile_image_url"] as! String)
         cell.profileImageView.af_setImage(withURL: imageUrl!)
         
+//        getting the date / time
+//        let twitterTimestamp = tweetArray[indexPath.row]["created_at"] as! String
+//        let tdf = DateFormatter()
+//        tdf.dateFormat = "EEE MMM dd HH:mm:ss Z yyyy"
+//        //tdf.doesRelativeDateFormatting = true
+//        let date = tdf.date(from: twitterTimestamp)
+//        if Date().timeIntervalSince(date!) > 24*60*60
+//        {
+//            tdf.dateStyle = .short
+//        }
+//        else
+//        {
+//            tdf.timeStyle = .short
+//        }
+//        let createdTime = tdf.string(from: date!)
+//        cell.tweetTime.text = createdTime
+
+        //get relative time for tweetTime
+        cell.tweetTime.text = getRelativeTime(timeString: (tweetArray[indexPath.row]["created_at"] as! String))
+        
+        //set boolean favorited
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        
+        //pass retweeted boolean
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
+        //pass id to cell
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        
         return cell
+    }
+    
+    func getRelativeTime(timeString: String) -> String {
+        let time: Date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
+        time = dateFormatter.date(from: timeString)!
+        return time.timeAgoDisplay()
     }
     
     @IBAction func onLogout(_ sender: Any) {
@@ -133,4 +178,28 @@ class HomeTableViewController: UITableViewController {
     }
     */
 
+}
+
+//extend date
+extension Date {
+    func timeAgoDisplay() -> String {
+        let secondsAgo = Int(Date().timeIntervalSince(self))
+        let minute = 60
+        let hour = 60 * minute
+        let day = 24 * hour
+        let week = 7 * day
+        
+        //check how long ago
+        if secondsAgo < minute {
+            return "\(secondsAgo) sec ago"
+        } else if secondsAgo < hour {
+            return "\(secondsAgo / minute) min ago"
+        } else if secondsAgo < day {
+            return "\(secondsAgo / hour) hrs ago"
+        } else if secondsAgo < week {
+            return "\(secondsAgo / day) days ago"
+        }
+        
+        return "\(secondsAgo / week) weeks ago"
+    }
 }
--- Twitter_starter/Twitter/TweetCell.swift
+++ Twitter_starter/Twitter/TweetCell.swift
@@ -13,6 +13,14 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContentLabel: UILabel!
+    @IBOutlet weak var tweetTime: UILabel!
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
+    
+    var tweetId:Int = -1
+    var favorited:Bool = false
+    var retweeted:Bool = false
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,5 +32,73 @@ class TweetCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func setFavorite(_ isFavorited:Bool)
+    {
+        favorited = isFavorited
+        if(favorited)
+        {
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }
+        else{
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool)
+    {
+        retweeted = isRetweeted
+        if(retweeted)
+        {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+        else
+        {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
 
+    @IBAction func favTweet(_ sender: Any) {
+        let tobeFavorited = !favorited
+        if tobeFavorited {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                //favorited & change image
+                self.setFavorite(true)
+            }, failure: { (Error) in
+                print("Favorite did not succeed: \(Error)")
+            })
+        }
+        else
+        {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                //no longer favorited & change image
+                self.setFavorite(false)
+            }, failure: { (Error) in
+                print("Unfavorite did not succeed: \(Error)")
+            })
+        }
+    }
+
+    @IBAction func reTweet(_ sender: Any) {
+        let tobeRetweeted = !retweeted
+        if tobeRetweeted {
+            TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+                //retweeted & change image
+                self.setRetweeted(true)
+            }, failure: { (Error) in
+                print("Retweet did not succeed: \(Error)")
+            })
+        }
+        else
+        {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                //no longer retweeted & change image
+                self.setRetweeted(false)
+            }, failure: { (Error) in
+                print("undo retweet did not succeed: \(Error)")
+            })
+        }
+    }
 }
